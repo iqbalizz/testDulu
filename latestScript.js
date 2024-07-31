366e3ce691f976c4d8c73b9d5a69f9471eb77790
iqbalizz <iqbalizzudin13@gmail.com>
2024-07-31 11:45:28 +0700

    Update latestScript.js

--- latestScript.js
+++ latestScript.js
@@ -1,3 +1,3 @@
 // JavaScript code here
 console.log('Ini adalah script terbaru.');
-console.log(`Halooo gaiss, sudah ada update terbaru lagi yaa`)
+console.log(`Halooo gaiss, sudah ada update terbaru lagi yaaa`)
